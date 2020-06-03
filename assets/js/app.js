0727e2bc6fceb1a30abdf05e1ed34a5c10dbcccb
Justine <pignet.justine@gmail.com>
2020-06-03 19:12:12 +0200

    debug Travis

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,6 +18,6 @@ require('bootstrap');
 // require('bootstrap/js/dist/tooltip');
 // require('bootstrap/js/dist/popover');
 
-$(document).ready(function() {
+$(document).ready(function () {
     $('[data-toggle="popover"]').popover();
 });
