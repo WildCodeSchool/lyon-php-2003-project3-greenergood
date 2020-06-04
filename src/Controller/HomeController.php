6f40a54f0d2d9cdadbb21532ab07f966073b6e00
Stéphane <steph@Asus.home>
2020-06-04 19:20:08 +0200

    Created NewBenevoleController and add a button on the hompage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController extends AbstractController
+class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="app_index")
