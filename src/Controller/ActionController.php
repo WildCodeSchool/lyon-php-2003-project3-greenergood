ac21f0c44762ee8da681bdc0671ef40c179ad1c5
benoit <lecerfbenoit@gmail.com>
2020-06-10 11:11:52 +0200

    Create CRUD Action

--- src/Controller/ActionController.php
+++ src/Controller/ActionController.php
@@ -5,9 +5,9 @@ namespace App\Controller;
 use App\Entity\Action;
 use App\Form\ActionType;
 use App\Repository\ActionRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -16,16 +16,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class ActionController extends AbstractController
 {
     /**
-     * @Route("/", name="index")
+     * @Route("/", name="index", methods={"GET"})
      */
-    public function index() :Response
+    public function index(ActionRepository $actionRepository): Response
     {
-        return $this->render('/action.html.twig');
+        return $this->render('action/index.html.twig', [
+            'actions' => $actionRepository->findAll(),
+        ]);
     }
-  
+
     /**
-     * Method used to add a new Action file to the database
-     *
      * @Route("/new", name="new", methods={"GET","POST"})
      */
     public function new(Request $request) :Response
@@ -54,5 +54,49 @@ class ActionController extends AbstractController
             'action' => $action,
             'form' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route("/{id}", name="show", methods={"GET"})
+     */
+    public function show(Action $action): Response
+    {
+        return $this->render('action/show.html.twig', [
+            'action' => $action,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Action $action): Response
+    {
+        $form = $this->createForm(ActionType::class, $action);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('action_index');
+        }
+
+        return $this->render('action/edit.html.twig', [
+            'action' => $action,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Action $action): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$action->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($action);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('action_index');
     }
 }
