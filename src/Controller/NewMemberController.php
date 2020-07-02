18db5cfd418318096cb0c9615d1682bf5e84c080
Justine <pignet.justine@gmail.com>
2020-07-02 18:12:04 +0200

    changed path on home page to welcome

---
+++ src/Controller/NewMemberController.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NewMemberController extends AbstractController
+{
+    /**
+     * @Route("/bienvenue", name="welcome")
+     */
+    public function index() : Response
+    {
+        return $this->render("welcome.html.twig");
+    }
+}
