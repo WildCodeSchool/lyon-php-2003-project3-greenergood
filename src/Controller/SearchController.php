5d98f8bd194ab20206513aac103a1c83ef14c16b
Stephane-acl <stephaneacl@gmail.com>
2020-07-07 16:28:25 +0200

    searchBar

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -3,23 +3,28 @@
 namespace App\Controller;
 
 use App\Form\SearchType;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SearchController extends AbstractController
 {
     /**
-     * @Route("/search", name="search")
+     * @Route("/search", name="search", methods={"GET","POST"})
+     * @param Request $request
      */
-    public function index()
+    public function searchBar(Request $request) :Response
     {
-        /*$form = $this->createForm(
+        $form = $this->createForm(
             SearchType::class,
             null,
             ['method' => Request::METHOD_GET]
-        );*/
+        );
 
-        return $this->render('_navbar.html.twig');
+        return $this->render('search/search.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
