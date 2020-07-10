cc298333185c100fbbbe8961b46eed1350645c5e
Matthieu Martinot <martinot.matthieu@gmail.com>
2020-07-10 16:48:55 +0200

    fix migrations

--- src/Migrations/Version20200707092026.php
+++ src/Migrations/Version20200707092026.php
@@ -20,10 +20,7 @@ final class Version20200707092026 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE action_method (action_id INT NOT NULL, method_id INT NOT NULL, INDEX IDX_9C690B0B9D32F035 (action_id), INDEX IDX_9C690B0B19883967 (method_id), PRIMARY KEY(action_id, method_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE action_method ADD CONSTRAINT FK_9C690B0B9D32F035 FOREIGN KEY (action_id) REFERENCES action (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE action_method ADD CONSTRAINT FK_9C690B0B19883967 FOREIGN KEY (method_id) REFERENCES method (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE method ADD category_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE method ADD CONSTRAINT FK_5E593A6012469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
         $this->addSql('CREATE INDEX IDX_5E593A6012469DE2 ON method (category_id)');
@@ -33,8 +30,6 @@ final class Version20200707092026 extends AbstractMigration
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE method DROP FOREIGN KEY FK_5E593A6012469DE2');
-        $this->addSql('DROP TABLE action_method');
         $this->addSql('DROP TABLE category');
         $this->addSql('DROP INDEX IDX_5E593A6012469DE2 ON method');
         $this->addSql('ALTER TABLE method DROP category_id');
